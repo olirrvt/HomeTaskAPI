4edc9e9fd915dd0d03a3f5bd3815852c8a782232
Taylor Oliveira <taylorso2004@hotmail.com>
2023-06-21 17:43:59 -0300

    Adicionando Controllers

---
+++ HomeTaskerAPI/Controllers/MoradorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeTaskerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoradorController : ControllerBase
+    {
+    }
+}
--- HomeTaskerAPI/HomeTaskerDbContext.cs
+++ HomeTaskerAPI/HomeTaskerDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HomeTaskerAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HomeTaskerAPI
 {
