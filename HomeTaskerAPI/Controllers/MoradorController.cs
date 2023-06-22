c66d3979a400d66b1908f07d0f3aebd2d41919bc
Taylor Oliveira <taylorso2004@hotmail.com>
2023-06-22 10:34:30 -0300

    Finalizando CRUD de moradores

--- HomeTaskerAPI/Controllers/MoradorController.cs
+++ HomeTaskerAPI/Controllers/MoradorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HomeTaskerAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,5 +22,104 @@ namespace HomeTaskerAPI.Controllers
 
             return moradores == null ? NotFound() : Ok(moradores); // Retorno do método
         }
+
+        [HttpGet]
+        [Route("Morador/{id}")]
+        public async Task<IActionResult> getByIdAsync(
+            [FromServices] HomeTaskerDbContext homeTaskerDbContext,
+            [FromRoute] int id)
+        {
+            var morador = await homeTaskerDbContext
+                .Moradores.AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == id);  // Pega o Primeiro registro que tenha o id solicitado
+
+            return morador == null ? NotFound() : Ok(morador);
+        }
+
+        [HttpPost]
+        [Route("Morador")]
+        public async Task<IActionResult> PostAsync(
+            [FromServices] HomeTaskerDbContext homeTaskerDbContext,
+            [FromBody] Moradore moradore
+            )
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await homeTaskerDbContext.Moradores.AddAsync(moradore);
+                await homeTaskerDbContext.SaveChangesAsync();
+                return Created($"Morador/morador/{moradore.Id}", moradore);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Morador/{id}")]
+        public async Task<IActionResult> PutAsync(
+            [FromServices] HomeTaskerDbContext homeTaskerDbContext,
+            [FromBody] Moradore moradore,
+            [FromRoute] int id )
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Model inválida");
+            }
+
+            var m = await homeTaskerDbContext.Moradores
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (m == null)
+            {
+                return NotFound("Morador não encontrado!");
+            }
+
+            try
+            {
+                m.Nome = moradore.Nome;
+
+                homeTaskerDbContext.Moradores.Update(m);
+                await homeTaskerDbContext.SaveChangesAsync();
+                return Ok(m);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("Morador/{id}")]
+        public async Task<IActionResult> DeleteAsync(
+            [FromServices] HomeTaskerDbContext homeTaskerDbContext,
+            [FromRoute] int id)
+        {
+            var mor = await homeTaskerDbContext.Moradores
+                .FirstOrDefaultAsync(mor => mor.Id == id);
+
+            if (mor == null)
+            {
+                return BadRequest("Morador não encontrado!");
+            }
+
+            try
+            {
+                homeTaskerDbContext.Moradores.Remove(mor);
+                await homeTaskerDbContext.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
